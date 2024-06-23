31e5d82025e011bc16f4b0fb1a04a398ccbacdd7
aksinghania <akshtsinghnia@gmail.com>
2024-06-23 23:18:20 +0530

    improved the camera feature

---
+++ src/utilities.js
@@ -0,0 +1,16 @@
+export const drawHand = (predictions, ctx) => {
+  if (predictions.length > 0) {
+    predictions.forEach((predictions) => {
+      const landmarks = predictions.landmarks;
+
+      for (let i = 0; i < landmarks.length; i++) {
+        const x = landmarks[i][0];
+        const y = landmarks[i][1];
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, 3 * Math.PI);
+        ctx.fillStyle = "indigo";
+        ctx.fill();
+      }
+    });
+  }
+};
