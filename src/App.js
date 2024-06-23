f07edae753b4eecf49c6b86bdb4ab2acd8b6aebc
aksinghania <akshtsinghnia@gmail.com>
2024-06-23 20:24:04 +0530

    coded the camera feature

--- src/App.js
+++ src/App.js
@@ -6,11 +6,28 @@ import Webcam from "react-webcam";
 import "./App.css";
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
   return (
     <div className="App">
       <header className="App-header">
         <Webcam
           ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
           style={{
             position: "absolute",
             marginLeft: "auto",
